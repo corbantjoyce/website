ecf65cb45562fce9dab001e165e48ea67d73726c
Corban Joyce <corbanjoyce@Corbans-MacBook-Pro.local>
2020-05-14 23:18:33 -0400

    added bootstrap cdn using helmet, added title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Helmet } from 'react-helmet';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+        <title>Home | Music | Places I've Been | Philanthropy | Videos | About</title>
+      </Helmet>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
