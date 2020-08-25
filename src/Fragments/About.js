2782f807a4bf65076dd0f3924c7a84cf41d01d8d
Corban Joyce <corbantjoyce@gmail.com>
2020-08-24 23:02:25 -0400

    updated paragraph font size

--- src/Fragments/About.js
+++ src/Fragments/About.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     p: {
-        fontSize: '2rem',
+        fontSize: '1.5rem',
     }
 }));
 
--- src/Fragments/Home.js
+++ src/Fragments/Home.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     p: {
-        fontSize: '1rem',
+        fontSize: '1.5rem',
     }
 }));
 
