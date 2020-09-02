9e438661fe8f2251c45680d8b73c546d3a40c490
Corban Joyce <corbantjoyce@gmail.com>
2020-09-01 23:11:42 -0400

    updated music page to include genius

--- src/Fragments/Home.js
+++ src/Fragments/Home.js
@@ -20,11 +20,11 @@ const Home = props => {
             <Grid container spacing={2}>
                 <Grid item xs={12}>
                     <p className={classes.p}>
-                        Are you registered to vote?  You can use this link if not:
-                        <a href="https://www.vote.gov" target="_blank" rel="noopener noreferrer"> vote.gov</a>.  It takes like 5 minutes.
+                        Welcome to my page!  It is currently a work in progress.  The tech stack used for this site is React and node.js, and if I implement a backend, I will either use MongoDB or DynamoDB.
                     </p>
                     <p className={classes.p}>
-                        Welcome to my page!  It is currently a work in progress.
+                        If you are a US citizen, make sure you are registered to vote:
+                        <a href="https://www.vote.gov" target="_blank" rel="noopener noreferrer"> vote.gov</a>.  It takes like 5 minutes.
                     </p>
                     <p className={classes.p}>
                         Change the background image at the top by clicking or tapping it.  All photos were taken on my iPhone.
--- src/Fragments/Music.js
+++ src/Fragments/Music.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
+import geniusprofile from '../images/unannotatedunmastered.jpeg';
 
 const useStyles = makeStyles((theme) => ({
     p: {
         marginTop: '1%',
         fontSize: '1.5rem',
+        justifyContent: 'center'
     }
 }));
 
@@ -21,10 +23,18 @@ const Music = props => {
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
                         <p className={classes.p}>
-                            This page is currently under construction.
+                            This page is currently under construction.  More music to come ;)
                         </p>
                     </Grid>
-                    <Grid item xs={12}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <img src={geniusprofile} style={{maxWidth: '100%', maxHeight: '100%'}}/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <p className={classes.p}>
+                                I have always loved music, as many people do.  My passion has driven me to analyze the meaning of music, and I became an editor for <a href="https://www.genius.com/unannotatedunmastered" target="_blank" rel="noopener noreferrer">Genius.com</a> in the spring of 2016.  My <a href="https://www.genius.com/unannotatedunmastered" target="_blank" rel="noopener noreferrer">profile</a> contains the majority of my work for the site.  While I used to be extremely active, making annotations and participating in forums, I have not been active in recent years.
+                            </p>
+                        </Grid>
                     </Grid>
                 </Grid>
             </div>
