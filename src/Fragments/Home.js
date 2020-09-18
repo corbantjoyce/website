6cb7b2508ab527fda3e5697df47a96a0ee620d9e
Corban Joyce <corbantjoyce@gmail.com>
2020-09-18 19:03:20 -0400

    added responsive font size from react-native

--- src/Fragments/Home.js
+++ src/Fragments/Home.js
@@ -3,10 +3,11 @@ import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Links from '../Fragments/Links';
 import Paper from '@material-ui/core/Paper';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 const useStyles = makeStyles((theme) => ({
     p: {
-        fontSize: '1.5rem',
+        fontSize: RFPercentage(5),
         justifyContent: 'center',
     }
 }));
