e5694a3fe0ec9da1197d1266ec18c7df6f015047
Corban Joyce <corbantjoyce@gmail.com>
2021-09-12 12:05:05 -0400

    fixed width sizing of Research

--- src/components/Research.js
+++ src/components/Research.js
@@ -8,12 +8,15 @@ import svcc from '../images/conference.jpeg';
 const useStyles = makeStyles((theme) => ({
     p: {
         fontSize: '1rem',
+        maxWidth: '100%'
     },
     container: {
         padding: '2%'
     },
     img: {
-        padding: '2%'
+        padding: '2%',
+        maxWidth: '80%',
+        borderRadius: '2px'
     }
 }));
 
@@ -22,19 +25,17 @@ const Research = props => {
     return (
         <>
             <Grid container className={classes.container}>
-                <Grid item xs={12}>
+                <Grid item>
                     <p className={classes.p}>
                         During my undergrad, I did research in the <a href="https://mnmdl.osu.edu/" target="_blank" rel="noopener noreferrer">Micro/Nano Multi-Physical Dynamics Lab (MNMDL)</a> at the Ohio State University.
                     </p>
                 </Grid>
-                <Grid item xs={12} className={classes.img}>
+                <Grid item>
                     <a href='https://kb.osu.edu/handle/1811/88692' target="_blank" rel="noopener noreferrer">
-                        <img src={thesis} alt="First page of thesis"  
-                            style={{maxWidth: '80%', borderRadius: '2px'}}
-                        />
+                        <img src={thesis} alt="First page of thesis" className={classes.img}/>
                     </a>
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item>
                     <p className={classes.p}>
                         I successfully defended my thesis titled <a href='https://kb.osu.edu/handle/1811/88692' target="_blank" rel="noopener noreferrer">Investigation of Metal Corrosion by In-Situ Electrochemical Force Microscopy</a> in December 2019.  Additional information, including a summary of my work, can be found on my <a href='https://linkedin.com/in/corban-joyce' target="_blank" rel="noopener noreferrer">LinkedIn</a>.
                     </p>
